a36940cda051e92abd5eb33a87598a5c0701d81e
Daniel Cueto <dcueto14.info@gmail.com>
2024-08-27 13:10:28 +0200

    Using astro-auto-import package integration for auto importing components to .mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,22 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()]
+  integrations: [
+    AutoImport({
+      imports: [
+        "./src/components/Intro.astro",
+        "./src/components/Note.astro",
+
+        // import { YouTube } from 'astro-embed'
+        { "astro-embed": ["YouTube"] },
+      ],
+    }),
+    react(),
+    mdx(),
+    tailwind()
+  ]
 });
